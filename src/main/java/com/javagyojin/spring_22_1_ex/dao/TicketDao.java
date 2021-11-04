3197665375e773dc1a432a4d77ecdd6685e61af0
ici-903-00 <ici-903-00@DESKTOP-9H8V5DQ>
2021-11-04 10:03:53 +0900

    Transaction ex01

---
+++ src/main/java/com/javagyojin/spring_22_1_ex/dao/TicketDao.java
@@ -0,0 +1,39 @@
+package com.javagyojin.spring_22_1_ex.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+
+import com.javagyojin.spring_22_1_ex.dto.TicketDto;
+
+public class TicketDao {
+
+	JdbcTemplate template;
+
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
+	
+	public void buyTicket(final TicketDto dto) {
+		
+		this.template.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				// TODO Auto-generated method stub
+				String query="insert into card (consumerId, amount) values (?, ?)";
+				PreparedStatement pstmt = con.prepareStatement(query);
+				pstmt.setString(1, dto.getConumerId());
+				pstmt.setString(2, dto.getAmount() );				
+				
+				return pstmt;
+				}
+			
+			});
+		
+	}
+	
+}
---
+++ src/main/java/com/javagyojin/spring_22_1_ex/dto/TicketDto.java
@@ -0,0 +1,22 @@
+package com.javagyojin.spring_22_1_ex.dto;
+
+public class TicketDto {
+	
+	private String conumerId;
+	private String amount;
+	
+	public String getConumerId() {
+		return conumerId;
+	}
+	public void setConumerId(String conumerId) {
+		this.conumerId = conumerId;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	
+	
+}
